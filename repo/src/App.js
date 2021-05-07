ce65c2cd5aa788e7fcad62f4ba4fcb8e78849392
Martin Gallegos <martin.gallegos@wizeline.com>
2021-05-06 18:33:00 -0700

    test 2

--- repo/src/App.js
+++ repo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Martin...
+          Martin galletas...
         </p>
         <a
           className="App-link"
